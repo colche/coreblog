13e9446bee2aa8f80ab7b7aadc8df846f3a51253
colche <colche@COLCHE-LAPTOP>
2017-05-22 23:26:19 +1000

    initial check-in

---
+++ coreblog/ClientApp/app/app.module.shared.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+
+import { AppComponent } from './components/app/app.component'
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { SampleComponent } from './components/sample/sample.component';
+
+
+export const sharedConfig: NgModule = {
+    bootstrap: [ AppComponent ],
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        CounterComponent,
+        FetchDataComponent,
+        HomeComponent,
+        SampleComponent
+    ],
+    imports: [
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'counter', component: CounterComponent },
+            { path: 'fetch-data', component: FetchDataComponent },
+            { path: '**', redirectTo: 'home' }
+        ]),
+        MdButtonModule, MdCheckboxModule,
+    ]
+};
---
+++ coreblog/ClientApp/app/components/sample/sample.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sample',
+    templateUrl: './sample.component.html'
+})
+export class SampleComponent {
+    public name = 'testing';
+
+}
